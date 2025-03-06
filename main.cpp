13e3c8ed61a88fd227573fba9c94ca8b80383228
s-dementiev <s.dementiev@vtei.edu.ua>
2025-03-06 11:19:32 +0300

    Create main.cpp
    Innitial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int date = 6;
+    std::string month = "March";
+    
+    std::cout << "Hello ICT groop!\n"
+    std::cout << "Today is " << month << " " << Date;
+    
+    return 0;
+}
